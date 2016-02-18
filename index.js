c761462ab5bcb39f507c97889f1b6a41c139bd21
arunthampi <arun.thampi@gmail.com>
2016-02-18 09:47:41 -0800

    remove unused arg

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var path = require('path');
 
-module.exports = function(robot, scripts) {
+module.exports = function(robot) {
   robot.loadFile(path.resolve(__dirname, "src", "scripts"), "heroku-commands.js");
 };
